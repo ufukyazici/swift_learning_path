2ab46c59162fc17c21b66d571ab552036f416f4c
ufukyazici <ufukyazici.dev@gmail.com>
2023-10-07 03:32:08 +0300

    Initial Commit

---
+++ SwiftLearningPath/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftLearningPath
+//
+//  Created by Ufuk YAZICI on 7.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftLearningPath/SwiftLearningPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLearningPathApp.swift
+//  SwiftLearningPath
+//
+//  Created by Ufuk YAZICI on 7.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLearningPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
