201dac9fc347bfb8a0bd746b7c8eea76cf28b0f2
ufukyazici <ufukyazici.dev@gmail.com>
2023-10-27 21:12:14 +0300

    Atomic Design

--- SwiftUIBasics/SwiftUIBasics/PackageLearn.swift
+++ SwiftUIBasics/SwiftUIBasics/PackageLearn.swift
@@ -12,10 +12,8 @@ import Kingfisher
 struct PackageLearn: View {
     private let imageUrl: String = "https://picsum.photos/200/300"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        KFImage(URL(string: imageUrl))
-            .fade(duration: 0.5)
-            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+        CircleAvatarTextView(imageUrl: imageUrl, imageText: "Veli")
+        
     }
 }
 
---
+++ SwiftUIBasics/SwiftUIBasics/View/CircleAvatarTextView.swift
@@ -0,0 +1,24 @@
+//
+//  CircleAvatarTextView.swift
+//  SwiftUIBasics
+//
+//  Created by Ufuk YAZICI on 27.10.2023.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct CircleAvatarTextView: View {
+    let imageUrl: String
+    let imageText: String
+    var body: some View {
+        VStack {
+            KFImage(URL(string: imageUrl))
+                .fade(duration: 0.5)
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            Text(imageText)
+        }
+    }
+}
+
+
