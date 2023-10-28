f5d04b94c16d12f10ef76084d91311eb790b1f1c
ufukyazici <ufukyazici.dev@gmail.com>
2023-10-28 15:46:30 +0300

    basic calculator

--- SwiftFromScratch/Calculator/Calculator/ViewController.swift
+++ SwiftFromScratch/Calculator/Calculator/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstNumber: UITextField!
+    
+    @IBOutlet weak var resultText: UILabel!
+    @IBOutlet weak var secondNumber: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func toAddButton(_ sender: Any) {
+        if let firstNum = Float(firstNumber.text!) {
+            if let secondNum = Float(secondNumber.text!){
+                resultText.text = "Result: \(firstNum+secondNum)"
+            }
+        }
+    }
+    @IBAction func toDivideButton(_ sender: Any) {
+        if let firstNum = Float(firstNumber.text!) {
+            if let secondNum = Float(secondNumber.text!){
+                resultText.text = "Result: \(firstNum/secondNum)"
+            }
+        }
+    }
+    
+    @IBAction func toMultiplyButton(_ sender: Any) {
+        if let firstNum = Float(firstNumber.text!) {
+            if let secondNum = Float(secondNumber.text!){
+                resultText.text = "Result: \(firstNum*secondNum)"
+            }
+        }
+    }
+    @IBAction func toSubstractButton(_ sender: Any) {
+        if let firstNum = Float(firstNumber.text!) {
+            if let secondNum = Float(secondNumber.text!){
+                resultText.text = "Result: \(firstNum-secondNum)"
+            }
+        }
+    }
 }
 
