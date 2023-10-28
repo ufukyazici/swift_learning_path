8a4446fd9741da2aab832f95d0b172f837834851
ufukyazici <ufukyazici.dev@gmail.com>
2023-10-28 15:31:52 +0300

    swift basics

--- SwiftFromScratch/Calculator/Calculator/ViewController.swift
+++ SwiftFromScratch/Calculator/Calculator/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  SwiftFromScratch
+//  Calculator
 //
 //  Created by Ufuk YAZICI on 28.10.2023.
 //
@@ -9,15 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var titleString: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func changeButton(_ sender: Any) {
-        titleString.text = "XD"
-    }
-    
+
 }
 
